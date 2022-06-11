a71cc88faef445266046cb1cf9f05bc3b4dae8ae
Hanekawa-san <captainduck.official@gmail.com>
2022-06-11 20:03:58 +0300

    commit

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,7 +3,7 @@ import React, {useEffect} from "react";
 import {AuthContext} from "../../App";
 import {t} from "react-native-tailwindcss";
 import tw  from 'twrnc';
-import { ImageBackground, StyleSheet} from "react-native";
+import { ImageBackground, StyleSheet, FlatList } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import LogRocket from "@logrocket/react-native";
 import axios from "axios";
@@ -16,9 +16,22 @@ export default function ListScreen({navigation}, model) {
     const { signOut } = React.useContext(AuthContext);
 
     const [list, setList] = React.useState([]);
-    const [deleteStyle, setDeleteStyle] = React.useState(style.delete);
     const [buttonStyle, setButtonStyle] = React.useState(style.button);
-    const [elementStyle, setElementStyle] = React.useState(style.element);
+    const Item = ({item}) => (<View style={style.elements}>
+        <Pressable style={tw`flex-1 bg-indigo-500 h-12 w-12 items-center justify-center`} onPress={read}
+                   onPressIn={() => current.setNativeProps({style:tw`flex-1 bg-slate-800 h-12 w-12 items-center justify-center`})}
+                   onPressOut={() => current.setNativeProps({style:tw`flex-1 bg-indigo-500 h-12 w-12 items-center justify-center`})}>
+            <Text style={[t.textWhite, t.fontMedium, t.text2xl]}>{item.id}</Text>
+        </Pressable>
+        <Pressable style={tw`w-12 h-12 bg-indigo-500 items-center flex-initial justify-center`} onPress={remove}
+                   onPressIn={() => setDeleteStyle(tw`w-12 h-12 bg-slate-800 items-center flex-initial justify-center`)}
+                   onPressOut={() => setDeleteStyle(tw`w-12 h-12 bg-indigo-500 items-center flex-initial justify-center`)}>
+            <ImageBackground source={{ uri: "https://static.wikia.nocookie.net/baldi-fanon/images/7/7a/B889B0A9-4717-4D37-9ADD-AB7B5E11FAEF.png/revision/latest?cb=20190226222946" }}
+                             resizeMode="contain"
+                             style={tw`justify-center w-full h-full`}>
+            </ImageBackground>
+        </Pressable>
+    </View>);
 
     React.useEffect(() => {
         const bootstrapAsync = async () => {
@@ -47,40 +60,21 @@ export default function ListScreen({navigation}, model) {
 
     return (
         <ScrollView>
-            {list.map((item) => { return (
-                <View style={style.elements}>
-                    <Pressable style={elementStyle} onPress={read}
-                               onPressIn={() => setElementStyle(style.buttonPressIn)}
-                               onPressOut={() => setElementStyle(style.element)}>
-                        <Text style={[t.textWhite, t.fontMedium, t.text2xl]}>{item.id}</Text>
-                    </Pressable>
-                    <Pressable style={deleteStyle} onPress={remove}
-                               onPressIn={() => setDeleteStyle(style.deletePressIn)}
-                               onPressOut={() => setDeleteStyle(style.delete)}>
-                        <ImageBackground source={{ uri: "https://freesvg.org/img/close-button.png" }}
-                                         resizeMode="contain"
-                                         style={tw`justify-center w-14 h-14 shadow-xl border-2`}>
-                        </ImageBackground>
-                    </Pressable>
-                </View>
-            );
-            })}
+            <FlatList data={list} renderItem={Item}/>
             <Pressable style={buttonStyle} onPress={create}
                        onPressIn={() => setButtonStyle(style.buttonPressIn)}
                        onPressOut={() => setButtonStyle(style.button)}>
                 <Text style={[t.textWhite, t.fontMedium, t.text2xl]}>Добавить</Text>
             </Pressable>
+
         </ScrollView>
     );
 }
 
 const style = {
-    button: tw`w-full h-30 rounded bg-slate-800 items-center flex justify-center mt-5`,
-    buttonPressIn: tw`w-full h-30 rounded bg-emerald-400 items-center flex justify-center mt-5`,
-    delete: tw`w-14 h-30 rounded bg-slate-800 items-center flex-initial mt-5`,
-    deletePressIn: tw`w-14 h-30 rounded bg-emerald-400 items-center flex-initial mt-5`,
-    element: tw`bg-indigo-500 h-50`,
-    elements: tw`flex items-baseline`,
+    button: tw`w-full h-12 rounded bg-slate-800 items-center flex justify-center mt-5`,
+    buttonPressIn: tw`w-full h-12 rounded bg-emerald-400 items-center flex justify-center mt-5`,
+    elements: tw`flex flex-nowrap flex-row mt-5`,
 }
 
 const styles = StyleSheet.create({
